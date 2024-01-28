4fb4d7c679f873266b717600831882f72b653f14
s26963 <s26963@pjwstk.edu.pl>
2024-01-28 15:45:54 +0100

    Added BankController.java

---
+++ src/main/java/com/example/s26963_bank/controller/BankController.java
@@ -0,0 +1,35 @@
+package com.example.s26963_bank.controller;
+
+import com.example.s26963_bank.exceptions.ValidationException;
+import com.example.s26963_bank.model.Client;
+import com.example.s26963_bank.service.BankService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/client")
+@AllArgsConstructor
+public class BankController {
+
+    private BankService bankService;
+
+    @PostMapping
+    public ResponseEntity<Client> addClient(@RequestBody Client client){
+        try{
+            Client result = bankService.add(client);
+
+            return ResponseEntity.status(201).body(result);
+        }catch (ValidationException e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients(){
+        List<Client> clientList = bankService.getAllClients();
+        return ResponseEntity.ok(clientList);
+    }
+}
