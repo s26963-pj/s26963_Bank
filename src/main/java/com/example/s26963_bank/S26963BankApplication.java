0bc4f157cf599287e1eba039db29a07983986396
s26963 <s26963@pjwstk.edu.pl>
2024-01-28 15:10:03 +0100

    Created project

---
+++ src/main/java/com/example/s26963_bank/S26963BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s26963_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26963BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S26963BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/s26963_bank/S26963BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s26963_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26963BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
