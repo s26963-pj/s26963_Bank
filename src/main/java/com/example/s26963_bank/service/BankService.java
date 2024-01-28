5ea3e34091ed4d82fc1e0a011ea84a51e9d2a7f3
s26963 <s26963@pjwstk.edu.pl>
2024-01-28 15:29:39 +0100

    Added BankService.java

---
+++ src/main/java/com/example/s26963_bank/service/BankService.java
@@ -0,0 +1,25 @@
+package com.example.s26963_bank.service;
+
+
+import com.example.s26963_bank.model.Client;
+import com.example.s26963_bank.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class BankService {
+
+    private ClientRepository clientRepository;
+
+    public Client add(Client client){
+
+       return clientRepository.addClient(client);
+    }
+
+    public List<Client> getAllClients(){
+        return clientRepository.getAllclients();
+    }
+}
