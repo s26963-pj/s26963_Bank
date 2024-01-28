df4d7ede51b970d028294b299159c71c2d0f6940
s26963 <s26963@pjwstk.edu.pl>
2024-01-28 15:14:10 +0100

    Added all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,14 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok:1.18.30")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testImplementation("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<Test> {
